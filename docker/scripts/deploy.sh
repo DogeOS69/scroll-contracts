d700b196c6a9c3be880ceca020b9144c504b0494
shu-unifra <shu@unifra.io>
2025-08-01 07:08:24 +0900

    skip simulate L1 and deploy L1

--- docker/scripts/deploy.sh
+++ docker/scripts/deploy.sh
@@ -22,14 +22,14 @@ echo "FOUNDRY_BYTECODE_HASH: $FOUNDRY_BYTECODE_HASH"
 echo "BATCH_SIZE: $BATCH_SIZE"
 
 # simulate L1
-echo ""
-echo "simulating on L1"
-forge script scripts/deterministic/DeployScroll.s.sol:DeployScroll --rpc-url "$L1_RPC_ENDPOINT" --sig "run(string,string)" "L1" "verify-config" 
+# echo ""
+# echo "simulating on L1"
+# forge script scripts/deterministic/DeployScroll.s.sol:DeployScroll --rpc-url "$L1_RPC_ENDPOINT" --sig "run(string,string)" "L1" "verify-config" 
 
-# deploy L1
-echo ""
-echo "deploying on L1"
-forge script scripts/deterministic/DeployScroll.s.sol:DeployScroll --rpc-url "$L1_RPC_ENDPOINT" --batch-size "${BATCH_SIZE}" --sig "run(string,string)" "L1" "verify-config" --broadcast --json 
+# # deploy L1
+# echo ""
+# echo "deploying on L1"
+# forge script scripts/deterministic/DeployScroll.s.sol:DeployScroll --rpc-url "$L1_RPC_ENDPOINT" --batch-size "${BATCH_SIZE}" --sig "run(string,string)" "L1" "verify-config" --broadcast --json 
 
 
 # simulate L2
